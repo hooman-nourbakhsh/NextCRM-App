fb0c9f7703bd8f3ff07a198c3180385a74479914
HoTaN <id4acc2014@protonmail.com>
2024-12-03 15:23:01 +0330

    fix: :rotating_light: import Customer model and update database connection
    I fixed a bug in pages/api/customer/index.js by importing the Customer model, which was previously missing.
    Additionally, I updated utils/connectDB.js to include mongoose.set("strictQuery", false) to suppress warnings related to the newer versions of Mongoose.
    After implementing these changes, I successfully tested the API using Postman, confirming that a new customer is being saved to the database without issues.

--- pages/api/customer/index.js
+++ pages/api/customer/index.js
@@ -1,3 +1,4 @@
+import Customer from "../../../models/Customer";
 import connectDB from "../../../utils/connectDB";
 
 export default async function handler(req, res) {
@@ -12,11 +13,11 @@ export default async function handler(req, res) {
     const data = req.body.data;
 
     if (!data.name || !data.lastName || !data.email)
-      return res.status(400).json({ status: "failed", message: "Please enter name, last name and email" });
-    
+      return res.status(400).json({ status: "failed", message: "Please enter name, lastName and email" });
+
     try {
       const customer = await Customer.create(data);
-      res.status({ status: "success", message: "Data saved successfully", data: customer });
+      res.status(201).json({ status: "success", message: "Data saved successfully", data: customer });
     } catch (error) {
       console.log(error);
       res.status(500).json({ status: "failed", message: "Error in creating new customer" });
