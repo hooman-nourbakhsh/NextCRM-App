8a1e1d7956df2b0a1350bff71efb51e0033ddde0
HoTaN <id4acc2014@protonmail.com>
2024-12-04 21:22:41 +0330

    feat: :sparkles: I implemented SSR for the homepage with database connection
    I implemented server-side rendering (SSR) for the homepage in pages/index.js.
    The page connects to the database using the connectDB utility and retrieves customer data from the Customer model.
    The fetched data is passed as props to the HomePage component, ensuring that the customer information is rendered on the server.
    Error handling is included to gracefully handle database connection or query failures, returning a 404 page if an error occurs.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1>NextCRM</h1>;
+import HomePage from "../components/template/HomePage";
+import Customer from "../models/Customer";
+import connectDB from "../utils/connectDB";
+
+function Index({ customers }) {
+  console.log(customers);
+  return <HomePage customers={customers} />;
+}
+export default Index;
+
+export async function getServerSideProps() {
+  try {
+    await connectDB();
+    const customers = await Customer.find();
+    return {
+      props: {
+        customers: JSON.parse(JSON.stringify(customers)),
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
